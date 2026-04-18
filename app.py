5234fe7ed47e9bb4f99bef7918bf0d08b43678dd
Finn <finnjorg@>
2026-04-18 15:47:22 +0200

    authen updated igen

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ st.set_page_config(
         layout="wide",
         initial_sidebar_state="expanded",
 )
-st.title("TEST")
+
 st.image("logo2.jpg")
 st.title("HAMMERKNUDEN SOMMERPENSION")
 status = login()
