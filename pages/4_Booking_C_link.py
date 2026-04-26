b77b7781ae11a13a184acab1e2c711fdf3af0786
Finn <finnjorg@>
2026-04-26 21:21:51 +0200

    update github

--- pages/4_Booking_C_link.py
+++ pages/4_Booking_C_link.py
@@ -4,4 +4,4 @@ from auth import require_login
 st.set_page_config(page_title="link", layout="wide")
 require_login()
 
-st.link_button("Booking_com","https//account.booking.com")
+st.link_button("Booking_com", "https://account.booking.com")
