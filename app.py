9f9f50088e68c0e529c3fe23b334e991d005a47c
Finn <finnjorg@>
2026-04-27 20:02:10 +0200

    login sat øverst

--- app.py
+++ app.py
@@ -5,11 +5,13 @@ st.set_page_config(
         layout="wide",
         initial_sidebar_state="expanded",
 )
+require_login()
+
 st.text("version 2.0.1")
 st.image("logo2.jpg")
 st.title("HAMMERKNUDEN SOMMERPENSION")
 
-#require_login()
+
 
 st.success(f"Velkommen {st.session_state.get('username')} 👋")
 st.info("Brug menuen i venstre side 👈")
--- pages/5_setup.py
+++ pages/5_setup.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from auth import require_login, require_admin
 st.set_page_config(page_title="Setup", layout="wide")
-#require_login()
+require_login()
 require_admin()
 
 st.title("Setup ⚙️")
