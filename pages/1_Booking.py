c31abf1894ac9fcea5d749b67273cf473e44e40d
Finn <finnjorg@>
2026-05-02 15:43:07 +0200

    week number and events 2026

--- pages/1_Booking.py
+++ pages/1_Booking.py
@@ -120,11 +120,14 @@ print(unique_values)
 st.markdown(f"**Antal ledige rum**  {ledige_rum}")  # "ledige} rum ", {ledige_rum})
 print(df)
 
+
 def highlight_cells(val):
 
     color = 'background-color: #66FF66' if val == 'va' else ''  # Grøn for 'va'
     return color
-styled_data = new_data[['dato', '1-I', '2-I', '3-I', '4-I', '5-I']].style.map(highlight_cells)
+
+
+styled_data = new_data[['dato', 'week_event', '1-I', '2-I', '3-I', '4-I', '5-I']].style.map(highlight_cells)
 st.dataframe(styled_data)
 col1, col2, = st.columns(2)
 with col1:
