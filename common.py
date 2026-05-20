5323c146c565c79f43cbbd50db60d3c8af044fdb
Finn <finnjorg@>
2026-05-20 23:05:19 +0200

    days.days ændret til days overordnet

--- common.py
+++ common.py
@@ -1,13 +1,14 @@
 import streamlit as st
 
+
 def init_session():
 
     defaults = {
         "reservation_number": "",
         "reservation_name": "",
-        "reservation_date": None,
-        "reservation_checkin_date": None,
-        "reservation_checkout_date": None,
+        "reservation_date": date.today(),
+        "reservation_checkin_date": date.today(),
+        "reservation_checkout_date": date.today(),
     }
 
     for key, value in defaults.items():
