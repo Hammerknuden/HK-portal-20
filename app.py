2fd6cde75a1851250c9a8cefff6afafe6d300b6f
Finn <finnjorg@>
2026-05-21 17:34:34 +0200

    app py auth

--- app.py
+++ app.py
@@ -16,7 +16,11 @@ if "authentication_status" not in st.session_state:
 if "username" not in st.session_state:
     st.session_state["username"] = None
 
-# 🔐 LOGIN HER
+# 🔥 DENNE MANGLEDE
+if "logout" not in st.session_state:
+    st.session_state["logout"] = None
+
+# LOGIN
 require_login()
 
 # hvis ikke logget ind → stop
@@ -28,4 +32,3 @@ st.image("logo2.jpg")
 
 st.success(f"Velkommen {st.session_state.get('username')} 👋")
 st.info("Brug menuen i venstre side 👈")
-
