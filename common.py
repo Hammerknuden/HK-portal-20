f3ae347abb8c488bfb559dd250e7e9912bc14b3c
Finn <finnjorg@>
2026-05-28 18:48:34 +0200

    fjerner def init fra booking.py

--- common.py
+++ common.py
@@ -6,7 +6,6 @@ def init_session():
     defaults = {
         "reservation_number": "",
         "reservation_name": "",
-        #"reservation_date": date.today(),
         "reservation_checkin_date": date.today(),
         "reservation_checkout_date": date.today(),
     }
@@ -14,4 +13,5 @@ def init_session():
     for key, value in defaults.items():
         if key not in st.session_state:
             st.session_state[key] = value
+
 
--- pages/1_Booking.py
+++ pages/1_Booking.py
@@ -22,19 +22,19 @@ sys.path.append(str(Path(__file__).resolve().parents[1]))
 st.set_page_config(page_title="Booking", layout="wide")
 
 
-def init_session():
+#def init_session():
 
-    defaults = {
-        "reservation_number": "",
-        "reservation_name": "",
-        #"reservation_date": date.today(),
-        "reservation_checkin_date": date.today(),
-        "reservation_checkout_date": date.today()
-    }
+#    defaults = {
+#        "reservation_number": "",
+#        "reservation_name": "",
+#        #"reservation_date": date.today(),
+#        "reservation_checkin_date": date.today(),
+#        "reservation_checkout_date": date.today()
+#    }
 
-    for key, value in defaults.items():
-        if key not in st.session_state:
-            st.session_state[key] = value
+#    for key, value in defaults.items():
+#        if key not in st.session_state:
+#            st.session_state[key] = value
 
 #byt evt init og login
 
