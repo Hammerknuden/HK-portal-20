0ea4e0fb9fabe5a74deb2fd3f94fd32d57b0e40d
Finn <finnjorg@>
2026-06-01 09:19:22 +0200

    session state fault 560

--- pages/1_Booking.py
+++ pages/1_Booking.py
@@ -55,7 +55,7 @@ network = st.selectbox("vælg lokal eller web ", options=["local", "URL"])
 
 now = st.date_input("booking dato")#, key='reservation_date')
 
-booking_number = st.text_input("booking nummer ", key="reservation_number")
+booking_number = st.text_input("booking_nummer ", key="reservation_number")
 st.write(st.session_state["reservation_number"])
 
 col1, col2 = st.columns(2)
