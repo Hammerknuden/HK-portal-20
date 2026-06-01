b550671df20381ecb28df992ab614c2b9005256a
Finn <finnjorg@>
2026-06-01 09:32:47 +0200

    session state fault 560 ændret i call number fjernet

--- pages/1_Booking.py
+++ pages/1_Booking.py
@@ -56,7 +56,6 @@ network = st.selectbox("vælg lokal eller web ", options=["local", "URL"])
 now = st.date_input("booking dato")#, key='reservation_date')
 
 booking_number = st.text_input("booking_nummer ", key="reservation_number")
-st.write(st.session_state["reservation_number"])
 
 col1, col2 = st.columns(2)
 with col1:
@@ -557,11 +556,11 @@ if send_data: #and booking_submitted:
 
 if st.button("Gem booking"):
 
-    st.session_state["reservation_number"] = booking_number
     st.session_state["reservation_name"] = name
     st.session_state["reservation_checkin_date"] = checkin_date
     st.session_state["reservation_checkout_date"] = checkout_date
 
+    st.session_state["saved_reservation_number"] = booking_number
     st.success("Booking gemt")
 #st.write("AFSENDER STATE")
 #st.write(st.session_state)
