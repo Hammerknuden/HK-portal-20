c9277062b69d64c9c1d8c42f5d51a5ec1386052d
Finn <finnjorg@>
2026-06-01 09:17:56 +0200

    session state fault 560

--- pages/1_Booking.py
+++ pages/1_Booking.py
@@ -557,8 +557,8 @@ if send_data: #and booking_submitted:
 
 if st.button("Gem booking"):
 
-    st.session_state["reservation_number"] = {booking_number}
-    st.session_state["reservation_name"] = reservation_name
+    st.session_state["reservation_number"] = booking_number
+    st.session_state["reservation_name"] = name
     st.session_state["reservation_checkin_date"] = checkin_date
     st.session_state["reservation_checkout_date"] = checkout_date
 
