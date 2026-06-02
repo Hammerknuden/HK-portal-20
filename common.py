111338d801ff5d054475a084112751a6cb1c65ca
Finn <finnjorg@>
2026-06-02 20:54:15 +0200

    nyt forsøg for session state at over leve sideskift

--- common.py
+++ common.py
@@ -4,10 +4,10 @@ import streamlit as st
 
 def init_session():
     defaults = {
-        "reservation_number": "",
-        "reservation_name": "",
-        "reservation_checkin_date": date.today(),
-        "reservation_checkout_date": date.today(),
+        "reservation_number"#: "",
+        "reservation_name": #"",
+        "reservation_checkin_date"#: date.today(),
+        "reservation_checkout_date"#: date.today(),
     }
 
     for key, value in defaults.items():
