7eedc6e567977298e7cd364a18bbfda143389cb6
Finn <finnjorg@>
2026-06-05 08:57:10 +0200

    tilbage til sep søge kriterier

--- pages/2_databaseopslag.py
+++ pages/2_databaseopslag.py
@@ -24,61 +24,63 @@ try:
 
 except Exception as e:
     st.error(f"Fejl: {e}")
+
 st.subheader("Database opslag 4 niveauer ")
 
+
+familie_navn = st.text_input("Familienavn")
+telefon = st.text_input("Telefon")
+email = st.text_input("Email")
+booking = st.text_input("Bookingnummer")
 search = st.text_input("Søg")
 
 if st.button("Søg"):
 
-    result = (
-        supabase
-        .table("historie")
-        .select("*")
-        .or_(
-            f"Familienavn.eq.{search},"
-            f"telefon.eq.{search},"
-            f"Email.eq.{search},"
-            f"booking.eq.{search}"
+    if familie_navn:
+
+        result = (
+            supabase
+            .table("historie")
+            .select("*")
+            .eq("Familienavn", familie_navn)
+            .execute()
         )
-        .execute()
-    )
 
-    st.dataframe(pd.DataFrame(result.data))
+        st.dataframe(pd.DataFrame(result.data))
 
-     #   familie_navn = st.text_input("family name")
-     #   telefon_nummer = st.text_input("telefon nummer med prefix")
-     #   email = st.text_input("email adresse ")
-     #   booking = st.text_input("booking nummer input")
+    elif telefon:
 
-      #  database_check = st.button("check for known person")
+        result = (
+            supabase
+            .table("historie")
+            .select("*")
+            .eq("telefon", telefon)
+            .execute()
+        )
 
-       # if database_check:
-        #    BASE_DIR = Path.cwd()
-         #   file_path = BASE_DIR / "data" / 'Database hammerknuden.xlsx'
-          #  df = pd.read_excel(file_path, sheet_name='Dtb', dtype={'familienavn': str})
-           # search_value = familie_navn
-            #pd.set_option("display.max_columns", None, )
-      #      rows1 = df[df['Familienavn'] == search_value]
-       #     df = pd.read_excel(file_path, sheet_name='Dtb', dtype={'telefon': str})
-        #    search_value = telefon_nummer
-         #   pd.set_option("display.max_columns", None,)
-          #  rows2 = df[df['telefon'] == search_value]
-           # df = pd.read_excel(file_path, sheet_name="Dtb", dtype={'Email': str})
-      #      search_value = email
-       #     pd.set_option("display.max_columns", None)
-        #    rows3 = df[df['Email'] == search_value]
-         #   df = pd.read_excel(file_path, sheet_name="Dtb", dtype={'booking': str})
-          #  search_value = booking
-    #        pd.set_option("display.max_columns", None)
-     #       rows4 = df[df['booking'] == search_value]
+        st.dataframe(pd.DataFrame(result.data))
 
-      #      if familie_navn:
-       #         st.dataframe(rows1)
-        #    if telefon_nummer:
-         #       st.dataframe(rows2)
-          #  if email:
-           #     st.dataframe(rows3)
-    #        elif booking:
-     #           st.dataframe(rows4)
-      #  else:
-       #     st.text(" not in Database")
+    elif email:
+
+        result = (
+            supabase
+            .table("historie")
+            .select("*")
+            .eq("Email", email)
+            .execute()
+        )
+
+        st.dataframe(pd.DataFrame(result.data))
+
+    elif booking:
+
+        result = (
+            supabase
+            .table("historie")
+            .select("*")
+            .eq("booking", int(booking))
+            .execute()
+        )
+
+        st.dataframe(pd.DataFrame(result.data))
+
