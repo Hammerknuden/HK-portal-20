b8994c79903b7e0107ecb7f8b3bd2202a564f660
Finn <finnjorg@>
2026-06-16 21:10:31 +0200

    excel fjernet fra ankomster

--- pages/3_Ankomster.py
+++ pages/3_Ankomster.py
@@ -14,131 +14,8 @@ st.set_page_config(page_title="Ankomster", layout="wide")
 
 st.subheader("Dagens ankomster ")
 # Date inputs fra Streamlit
-year = st.selectbox("booking år", ["2026", "2027"])
-check_dato_start = st.date_input("Start dato")
-antal_dage = st.number_input("Antal dage", min_value=1, value=3)
-check_dato_slut = check_dato_start + timedelta(days=antal_dage)
-check_for_ankomst = st.button(" check ankomster")
-#check_dato_slut = st.date_input("Slut dato")
 
 
-if check_for_ankomst and year == "2026":
-    # Load Excel
-    BASE_DIR = Path.cwd()
-    file_path = BASE_DIR / "data" / "2026_BOOKING 10.xlsx"
-    df = pd.read_excel(file_path, sheet_name="ankomster")
-
-    # Rens kolonnenavne (vigtigt!)
-    df.columns = df.columns.str.strip().str.lower()
-
-    # Sikr at 'dato' findes
-    if 'dato' not in df.columns:
-        st.error(f"Kolonnen 'dato' findes ikke. Fundet kolonner: {df.columns.tolist()}")
-    else:
-        # Konverter til datetime
-        df['dato'] = pd.to_datetime(df['dato'], errors='coerce')
-
-        # Fjern rækker med ugyldige datoer
-        df = df.dropna(subset=['dato'])
-
-    # Filtrer på interval
-    filtreret_df = df[
-        (df['dato'].dt.date >= check_dato_start) &
-        (df['dato'].dt.date <= check_dato_slut)
-        ]
-
-        # Vis resultat
-    st.write("Ankomst oversigt:")
-    st.dataframe(filtreret_df)
-
-    BASE_DIR = Path.cwd()
-    file_path = BASE_DIR / "data" / '2026_BOOKING 10.xlsx'
-
-    #file_name = "2026_BOOKING 10.xlsx"
-    df = pd.read_excel(file_path, sheet_name="ankomst navn")
-
-    # Rens kolonnenavne (vigtigt!)
-    df.columns = df.columns.str.strip().str.lower()
-
-    # Sikr at 'dato' findes
-    if 'dato' not in df.columns:
-        st.error(f"Kolonnen 'dato' findes ikke. Fundet kolonner: {df.columns.tolist()}")
-    else:
-        # Konverter til datetime
-        df['dato'] = pd.to_datetime(df['dato'], errors='coerce')
-
-        # Fjern rækker med ugyldige datoer
-        df = df.dropna(subset=['dato'])
-
-    # Filtrer på interval
-    filtreret_df = df[
-        (df['dato'].dt.date >= check_dato_start) &
-        (df['dato'].dt.date <= check_dato_slut)
-        ]
-
-
-    # Vis resultat
-    st.write("Navne på ankomster:")
-    st.dataframe(filtreret_df)
-
-if check_for_ankomst and year == 2027:
-    
-    # Load Excel
-    BASE_DIR = Path.cwd()
-    file_path = BASE_DIR / "data" / '2027_BOOKING 10.xlsx'
-    #file_name = "2027_BOOKING 10.xlsx"
-    df = pd.read_excel(file_path, sheet_name="ankomster")
-
-    # Rens kolonnenavne (vigtigt!)
-    df.columns = df.columns.str.strip().str.lower()
-
-    # Sikr at 'dato' findes
-    if 'dato' not in df.columns:
-        st.error(f"Kolonnen 'dato' findes ikke. Fundet kolonner: {df.columns.tolist()}")
-    else:
-        # Konverter til datetime
-        df['dato'] = pd.to_datetime(df['dato'], errors='coerce')
-
-        # Fjern rækker med ugyldige datoer
-        df = df.dropna(subset=['dato'])
-
-        # Filtrer på interval
-        filtreret_df = df[
-            (df['dato'].dt.date >= check_dato_start) &
-            (df['dato'].dt.date <= check_dato_slut)
-            ]
-
-        # Vis resultat
-        st.write("Ankomst oversigt:")
-        st.dataframe(filtreret_df)
-
-        BASE_DIR = Path.cwd()
-        file_path = BASE_DIR / "data" / '2027_BOOKING 10.xlsx'
-        #file_name = "2026_BOOKING 10.xlsx"
-        df = pd.read_excel(file_path, sheet_name="ankomst navn")
-
-        # Rens kolonnenavne (vigtigt!)
-        df.columns = df.columns.str.strip().str.lower()
-
-        # Sikr at 'dato' findes
-        if 'dato' not in df.columns:
-            st.error(f"Kolonnen 'dato' findes ikke. Fundet kolonner: {df.columns.tolist()}")
-        else:
-            # Konverter til datetime
-            df['dato'] = pd.to_datetime(df['dato'], errors='coerce')
-
-            # Fjern rækker med ugyldige datoer
-            df = df.dropna(subset=['dato'])
-
-            # Filtrer på interval
-            filtreret_df = df[
-                (df['dato'].dt.date >= check_dato_start) &
-                (df['dato'].dt.date <= check_dato_slut)
-                ]
-
-            # Vis resultat
-            st.write("Navne på ankomster:")
-            st.dataframe(filtreret_df)
 #######
 #Supabase
 #######
@@ -157,7 +34,7 @@ check_dato_start = date.today()
 
 antal_dage = st.selectbox(
     "Vis ankomster de næste dage",
-    [3, 5],
+    [3, 5, 7, 9, 14],
     index=0
 )
 
