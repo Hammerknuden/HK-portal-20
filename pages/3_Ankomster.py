696b4761070dd8e609504415d879d6f4126d8f3a
Finn <finnjorg@>
2026-06-19 12:27:54 +0200

    ankomst opdateret med flere koloner

--- pages/3_Ankomster.py
+++ pages/3_Ankomster.py
@@ -44,7 +44,7 @@ check_dato_slut = check_dato_start + timedelta(days=antal_dage)
 result = (
     supabase.table("hk_dtb")
     .select(
-        "booking_number, navn, checkin_date, ankomst, bed, known, room_number"
+        "booking_number, navn, checkin_date, ankomst, bed, known, room_number, nation, enkelt"
     )
     .gte("checkin_date", str(check_dato_start))
     .lte("checkin_date", str(check_dato_slut))
