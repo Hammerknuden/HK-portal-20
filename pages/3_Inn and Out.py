1ed8be5f3e0d6ea16c9980868cab15cb906b5f7e
Finn <finnjorg@>
2026-06-23 08:37:14 +0200

    skifter print buttom

--- pages/3_Inn and Out.py
+++ pages/3_Inn and Out.py
@@ -137,23 +137,11 @@ if not df.empty:
 else:
     st.info("Ingen udcheckninger i perioden.")
 
-st.components.v1.html(
-    """
-    <button
-        onclick="window.print()"
-        style="
-            background-color:#4CAF50;
-            color:white;
-            padding:10px 20px;
-            border:none;
-            border-radius:5px;
-            cursor:pointer;
-            font-size:16px;
-        ">
-        🖨️ Print rapport
-    </button>
-    """,
-    height=60,
+st.download_button(
+    "📄 Download rapport",
+    pdf_bytes,
+    file_name="rapport.pdf",
+    mime="application/pdf"
 )
 
 #st.write( " for print brug Ctrl + P husk at lukke sidebar først")
