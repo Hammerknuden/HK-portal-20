cad78e78f3dabaab9a0d8d2b95be0e80ab139ddc
Finn <finnjorg@>
2026-07-13 08:28:51 +0200

    overskrift morgenmad

--- pages/5_breakfirst.py
+++ pages/5_breakfirst.py
@@ -12,7 +12,7 @@ from supabase import create_client
 st.set_page_config(page_title="Cleaning plan", layout="wide")
 require_login()
 
-st.subheader("suspended - under cunstruktion ")
+st.subheader("Antal personer til morgenmad ")
 
 load_dotenv()
 
